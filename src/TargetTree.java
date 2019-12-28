25964e3081929ade83a88b75b332e3e895fc21ae
YeYaBo <644631077@qq.com>
2019-12-28 19:01:14 +0800

    2019/10/22  添加极小化极大的类别  练习了部分题目

--- src/TargetTree.java
+++ src/TargetTree.java
@@ -538,7 +538,52 @@ public class TargetTree {
     }
     //endregion
 
+    //region 1008. 先序遍历构造二叉树  2017/11/18  前序遍历
+    /**
+     * 返回与给定先序遍历 preorder 相匹配的二叉搜索树（binary search tree）的根结点。
+     *
+     * (回想一下，二叉搜索树是二叉树的一种，其每个节点都满足以下规则，
+     * 对于 node.left 的任何后代，
+     * 值总 < node.val，而 node.right 的任何后代，值总 > node.val。
+     * 此外，先序遍历首先显示节点的值，然后遍历 node.left，接着遍历 node.right。
+     * 示例：
+     * 输入：[8,5,1,7,10,12]
+     * 输出：[8,5,10,1,7,null,12]
+     * 提示：
+     * 1 <= preorder.length <= 100
+     * 先序 preorder 中的值是不同的。
+     * @param preorder
+     * @return
+     */
+    public TreeNode bstFromPreorder(int[] preorder) {
+        if(preorder.length==0){
+            return null;
+        }
+        TreeNode res=new TreeNode(preorder[0]);
+        int left=0;
+        int right=0;
+        for(int i=1;i<preorder.length;i++){
+            if(preorder[i]<preorder[0]){
+                left++;
+                continue;
+            }else{
+                break;
+            }
+        }
+        right=preorder.length-left-1;
+        int[] leftpreorder=new int[left];
+        System.arraycopy(preorder,1,leftpreorder,0,left);
+        int[] rightpreorder=new int[right];
+        System.arraycopy(preorder,left+1,rightpreorder,0,right);
+        res.left=bstFromPreorder(leftpreorder);
+        res.right=bstFromPreorder(rightpreorder);
+        return res;
+    }
+    //endregion
+
     public static void main(String[] args) {
+        int[] pre=new int[]{8,5,1,7,10,12};
+        TreeNode treeNode=new TargetTree().bstFromPreorder(pre);
     }
 
 }
