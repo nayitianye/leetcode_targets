53697d59520996be03f6937cef88a3b575b84158
nayitianye <644631077@qq.com>
2021-05-15 23:48:44 +0800

    12. 整数转罗马数字 13. 罗马数字转整数

--- src/TargetString.java
+++ src/TargetString.java
@@ -7,6 +7,130 @@ import java.util.*;
  */
 public class TargetString {
 
+    // region 12. 整数转罗马数字  2021/5/15
+    /**
+     * 罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+     * 字符          数值
+     * I             1
+     * V             5
+     * X             10
+     * L             50
+     * C             100
+     * D             500
+     * M             1000
+     * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+     * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+     * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+     * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+     * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+     * 给你一个整数，将其转为罗马数字。
+     *
+     * 示例 1:
+     * 输入: num = 3
+     * 输出: "III"
+     * 示例 2:
+     * 输入: num = 4
+     * 输出: "IV"
+     * 示例 3:
+     * 输入: num = 9
+     * 输出: "IX"
+     * 示例 4:
+     * 输入: num = 58
+     * 输出: "LVIII"
+     * 解释: L = 50, V = 5, III = 3.
+     * 示例 5:
+     * 输入: num = 1994
+     * 输出: "MCMXCIV"
+     * 解释: M = 1000, CM = 900, XC = 90, IV = 4. 
+     *
+     * 提示：
+     * 1 <= num <= 3999
+     * @param num
+     * @return
+     */
+    public String intToRoman(int num) {
+        int[] values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] rom={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<values.length;i++){
+            while(num>=values[i]){
+                sb.append(rom[i]);
+                num-=values[i];
+            }
+        }
+        return sb.toString();
+    }
+    // endregion
+
+    // region 13. 罗马数字转整数 2021/5/15
+    /**
+     * 罗马数字包含以下七种字符: I，V，X，L，C，D 和 M。
+     * 字符          数值
+     * I             1
+     * V             5
+     * X             10
+     * L             50
+     * C             100
+     * D             500
+     * M             1000
+     * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+     * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+     * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+     * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+     * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+     * 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
+     *
+     * 示例 1:
+     * 输入: "III"
+     * 输出: 3
+     * 示例 2:
+     * 输入: "IV"
+     * 输出: 4
+     * 示例 3:
+     * 输入: "IX"
+     * 输出: 9
+     * 示例 4:
+     * 输入: "LVIII"
+     * 输出: 58
+     * 解释: L = 50, V= 5, III = 3.
+     * 示例 5:
+     * 输入: "MCMXCIV"
+     * 输出: 1994
+     * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+     *  
+     *
+     * 提示：
+     * 1 <= s.length <= 15
+     * s 仅含字符 ('I', 'V', 'X', 'L', 'C', 'D', 'M')
+     * 题目数据保证 s 是一个有效的罗马数字，且表示整数在范围 [1, 3999] 内
+     * 题目所给测试用例皆符合罗马数字书写规则，不会出现跨位等情况。
+     * IL 和 IM 这样的例子并不符合题目要求，49 应该写作 XLIX，999 应该写作 CMXCIX 。
+     * 关于罗马数字的详尽书写规则，可以参考 罗马数字 - Mathematics 。
+     * @param s
+     * @return
+     */
+    public int romanToInt(String s) {
+      HashMap<Character,Integer>romanToInt=new HashMap<Character,Integer>();
+      romanToInt.put('I',1);
+      romanToInt.put('V',5);
+      romanToInt.put('X',10);
+      romanToInt.put('L',50);
+      romanToInt.put('C',100);
+      romanToInt.put('D',500);
+      romanToInt.put('M',1000);
+      int res=0;
+      for(int i=0;i<s.length();i++){
+          int temp=romanToInt.get(s.charAt(i));
+          if(i<s.length()-1 && temp<romanToInt.get(s.charAt(i+1))){
+              res-=temp;
+          }else{
+              res+=temp;
+          }
+      }
+      return res;
+    }
+    //endregion
+
     //region 17. 电话号码的字母组合  2019/10/11 回溯求解
     //    /**
     //     * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
