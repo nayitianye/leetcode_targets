68a54a09e5ed4be99bba2a67dfb5b503e1ba8c00
nayitianye <644631077@qq.com>
2021-05-17 23:06:59 +0800

    993. 二叉树的堂兄弟节点

--- src/TargetTree.java
+++ src/TargetTree.java
@@ -538,6 +538,73 @@ public class TargetTree {
     }
     //endregion
 
+    // region  993. 二叉树的堂兄弟节点  2021/5/17
+    /**
+     * 在二叉树中，根节点位于深度 0 处，每个深度为 k 的节点的子节点位于深度 k+1 处。
+     * 如果二叉树的两个节点深度相同，但 父节点不同 ，则它们是一对堂兄弟节点。
+     * 我们给出了具有唯一值的二叉树的根节点 root ，以及树中两个不同节点的值 x 和 y 。
+     * 只有与值 x 和 y 对应的节点是堂兄弟节点时，才返回 true 。否则，返回 false。
+     * 示例 1：
+     * 输入：root = [1,2,3,4], x = 4, y = 3
+     * 输出：false
+     * 示例 2：
+     * 输入：root = [1,2,3,null,4,null,5], x = 5, y = 4
+     * 输出：true
+     * 示例 3：
+     * 输入：root = [1,2,3,null,4], x = 2, y = 3
+     * 输出：false
+     *
+     */
+    // x 的信息
+    int x;
+    TreeNode xParent;
+    int xDepth;
+    boolean xFound = false;
+
+    // y 的信息
+    int y;
+    TreeNode yParent;
+    int yDepth;
+    boolean yFound = false;
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        this.x = x;
+        this.y = y;
+        dfs(root, 0, null);
+        return xDepth == yDepth && xParent != yParent;
+    }
+
+    public void dfs(TreeNode node, int depth, TreeNode parent) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.val == x) {
+            xParent = parent;
+            xDepth = depth;
+            xFound = true;
+        } else if (node.val == y) {
+            yParent = parent;
+            yDepth = depth;
+            yFound = true;
+        }
+
+        // 如果两个节点都找到了，就可以提前退出遍历
+        // 即使不提前退出，对最坏情况下的时间复杂度也不会有影响
+        if (xFound && yFound) {
+            return;
+        }
+
+        dfs(node.left, depth + 1, node);
+
+        if (xFound && yFound) {
+            return;
+        }
+
+        dfs(node.right, depth + 1, node);
+    }
+    // endregion
+
     //region 1008. 先序遍历构造二叉树  2017/11/18  前序遍历
     /**
      * 返回与给定先序遍历 preorder 相匹配的二叉搜索树（binary search tree）的根结点。
