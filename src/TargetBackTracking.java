0c2abadaf7b83a9ce8a9e3e1ceeb15b74c303b7f
nayitianye <644631077@qq.com>
2021-05-20 20:45:17 +0800

    去除多余的空格

--- src/TargetBackTracking.java
+++ src/TargetBackTracking.java
@@ -54,9 +54,9 @@ public class TargetBackTracking {
 
     //region 22. 括号生成  2019/10/6  回溯求解
     /**
-     * 给出 n 代表生成括号的对数，请你写出一个函数，
+     * 给出n代表生成括号的对数，请你写出一个函数，
      * 使其能够生成所有可能的并且有效的括号组合。
-     * 例如，给出 n = 3，生成结果为：
+     * 例如，给出n = 3，生成结果为：
      *
      * [
      *   "((()))",
@@ -90,13 +90,13 @@ public class TargetBackTracking {
 
     //region 39. 组合总和  2019/10/11  回溯求解
     /**
-     * 给定一个无重复元素的数组 candidates 和一个目标数 target ，
-     * 找出 candidates 中所有可以使数字和为 target 的组合。
-     * candidates 中的数字可以无限制重复被选取。
+     * 给定一个无重复元素的数组candidates和一个目标数target，
+     * 找出candidates中所有可以使数字和为target的组合。
+     * candidates中的数字可以无限制重复被选取。
      * 说明：
-     * 所有数字（包括 target）都是正整数。
-     * 解集不能包含重复的组合。 
-     * 示例 1:
+     * 所有数字（包括target）都是正整数。
+     * 解集不能包含重复的组合。
+     * 示例1:
      * 输入: candidates = [2,3,6,7], target = 7,
      * 所求解集为:
      * [
@@ -104,13 +104,13 @@ public class TargetBackTracking {
      *   [2,2,3]
      * ]
      *
-     * 示例 2:
+     * 示例2:
      * 输入: candidates = [2,3,5], target = 8,
      * 所求解集为:
      * [
-     *   [2,2,2,2],
-     *   [2,3,3],
-     *   [3,5]
+     *  [2,2,2,2],
+     *  [2,3,3],
+     *  [3,5]
      * ]
      * @param candidates
      * @param target
@@ -137,14 +137,14 @@ public class TargetBackTracking {
 
     //region 40. 组合总和 II  2019/10/11  回溯求解
     /**
-     * 给定一个数组 candidates 和一个目标数 target ，
-     * 找出 candidates 中所有可以使数字和为 target 的组合。
-     * candidates 中的每个数字在每个组合中只能使用一次。
+     * 给定一个数组candidates和一个目标数target，
+     * 找出candidates中所有可以使数字和为target的组合。
+     * candidates中的每个数字在每个组合中只能使用一次。
      * 说明：
      * 所有数字（包括目标数）都是正整数。
-     * 解集不能包含重复的组合。 
-     * 示例 1:
-     * 输入: candidates = [10,1,2,7,6,1,5], target = 8,
+     * 解集不能包含重复的组合。
+     * 示例1:
+     * 输入: candidates =[10,1,2,7,6,1,5], target =8,
      * 所求解集为:
      * [
      *   [1, 7],
@@ -153,12 +153,12 @@ public class TargetBackTracking {
      *   [1, 1, 6]
      * ]
      *
-     * 示例 2:
-     * 输入: candidates = [2,5,2,1,2], target = 5,
+     * 示例2:
+     * 输入: candidates =[2,5,2,1,2], target =5,
      * 所求解集为:
      * [
-     *   [1,2,2],
-     *   [5]
+     *  [1,2,2],
+     *  [5]
      * ]
      * @param candidates
      * @param target
@@ -247,8 +247,8 @@ public class TargetBackTracking {
      * 输出: ["12345"]
      *
      * 注意：
-     * S 的长度不超过12。
-     * S 仅由数字和字母组成。
+     * S的长度不超过12。
+     * S仅由数字和字母组成。
      * @param S
      * @return
      */
