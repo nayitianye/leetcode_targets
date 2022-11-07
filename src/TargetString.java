49bb76f655735cfc212d908836ec481be846016a
nayitian <644631077@qq.com>
2022-11-07 23:24:13 +0800

    459. 重复的子字符串

--- src/TargetString.java
+++ src/TargetString.java
@@ -317,6 +317,35 @@ public class TargetString {
     }
     //endregion
 
+    //region 459. 重复的子字符串  2022.11.07 字符串处理
+    /**
+     * 给定一个非空的字符串 s ，检查是否可以通过由它的一个子串重复多次构成。
+     * 示例 1:
+     * 输入: s = "abab"
+     * 输出: true
+     * 解释: 可由子串 "ab" 重复两次构成。
+     *
+     * 示例 2:
+     * 输入: s = "aba"
+     * 输出: false
+     *
+     * 示例 3:
+     * 输入: s = "abcabcabcabc"
+     * 输出: true
+     *
+     * 解释: 可由子串 "abc" 重复四次构成。 (或子串 "abcabc" 重复两次构成。)
+     * 提示：
+     * 1 <= s.length <= 10^4
+     * s 由小写英文字母组成
+     * @param s
+     * @return
+     */
+    public boolean repeatedSubstringPattern(String s) {
+        String str=s+s;
+        return str.substring(1,str.length()-1).contains(s);
+    }
+    //endregion
+
     //region 521. 最长特殊序列 Ⅰ  2019/10/6  文字游戏
     /**
      * 给定两个字符串，你需要从这两个字符串中找出最长的特殊序列。
