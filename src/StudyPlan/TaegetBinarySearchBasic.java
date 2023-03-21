acf431115ce106106797e7af581da57ec5a81507
nayitianye <644631066@qq.com>
2023-03-21 19:49:15 +0800

    学习计划_二分查找基础: 658. 找到 K 个最接近的元素

--- src/StudyPlan/TaegetBinarySearchBasic.java
+++ src/StudyPlan/TaegetBinarySearchBasic.java
@@ -1,7 +1,9 @@
 package StudyPlan;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author yyb
@@ -10,13 +12,14 @@ import java.util.HashMap;
  */
 public class TaegetBinarySearchBasic {
 
-    //region    202303020   209. 长度最小的子数组
+    //region    20230320    209. 长度最小的子数组
 
     /**
      * https://leetcode.cn/problems/minimum-size-subarray-sum/
-     * @param target  一个正整数 target
-     * @param nums  含有 n 个正整数的数组 nums
-     * @return  该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度
+     *
+     * @param target 一个正整数 target
+     * @param nums   含有 n 个正整数的数组 nums
+     * @return 该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度
      */
     public int minSubArrayLen(int target, int[] nums) {
         int[] presum = new int[nums.length + 1];
@@ -28,7 +31,7 @@ public class TaegetBinarySearchBasic {
         }
         int minLength = Integer.MAX_VALUE;
         for (int i = 1; i < presum.length; i++) {
-            int temptarget = target + presum[i-1];
+            int temptarget = target + presum[i - 1];
             int bound = Arrays.binarySearch(presum, temptarget);
             if (bound < 0) {
                 bound = -bound - 1;
@@ -38,6 +41,51 @@ public class TaegetBinarySearchBasic {
             }
         }
         return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+    //endregion
+
+    //region    20230321    658. 找到 K 个最接近的元素
+
+    /**
+     * https://leetcode.cn/problems/find-k-closest-elements/
+     *
+     * @param arr 排序好 的数组 arr
+     * @param k   整数 k
+     * @param x   整数 x
+     * @return 从数组中找到最靠近 x（两数之差最小）的 k 个数
+     */
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int right = binarySearch(arr, x);
+        int left = right - 1;
+        while (k-- > 0) {
+            if (left < 0) {
+                right++;
+            } else if (right >= arr.length) {
+                left--;
+            } else if (x - arr[left] <= arr[right] - x) {
+                left--;
+            } else {
+                right++;
+            }
+        }
+        List<Integer> ans = new ArrayList<Integer>();
+        for (int i = left + 1; i < right; i++) {
+            ans.add(arr[i]);
+        }
+        return ans;
+    }
+
+    public int binarySearch(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= x) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
     }
     //endregion
 
