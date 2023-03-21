29522ec1bfa9cb543b45bb9c6748cbfca41f3155
someday <644631077@qq.com>
2023-03-22 00:15:55 +0800

    学习计划_二分查找基础: 611. 有效三角形的个数 658. 找到 K 个最接近的元素 1894. 找到需要补充粉笔的学生编号

---
+++ src/LeetBook/TargetSortAlgorithms.java
@@ -0,0 +1,11 @@
+package LeetBook;
+
+/**
+ * @author yyb
+ * leetcode_leetbook_sort_algorithms
+ * leetbook 排序算法图文学
+ * @url https://leetcode.cn/leetbook/detail/sort-algorithms/
+ */
+public class TargetSortAlgorithms {
+
+}
--- src/StudyPlan/TaegetBinarySearchBasic.java
+++ src/StudyPlan/TaegetBinarySearchBasic.java
@@ -1,7 +1,10 @@
 package StudyPlan;
 
+import java.nio.file.ClosedWatchServiceException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author yyb
@@ -10,13 +13,14 @@ import java.util.HashMap;
  */
 public class TaegetBinarySearchBasic {
 
-    //region    202303020   209. 长度最小的子数组
+    //region    20230320    209. 长度最小的子数组
 
     /**
      * https://leetcode.cn/problems/minimum-size-subarray-sum/
-     * @param target  一个正整数 target
-     * @param nums  含有 n 个正整数的数组 nums
-     * @return  该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度
+     *
+     * @param target 一个正整数 target
+     * @param nums   含有 n 个正整数的数组 nums
+     * @return 该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度
      */
     public int minSubArrayLen(int target, int[] nums) {
         int[] presum = new int[nums.length + 1];
@@ -28,7 +32,7 @@ public class TaegetBinarySearchBasic {
         }
         int minLength = Integer.MAX_VALUE;
         for (int i = 1; i < presum.length; i++) {
-            int temptarget = target + presum[i-1];
+            int temptarget = target + presum[i - 1];
             int bound = Arrays.binarySearch(presum, temptarget);
             if (bound < 0) {
                 bound = -bound - 1;
@@ -41,7 +45,119 @@ public class TaegetBinarySearchBasic {
     }
     //endregion
 
+    //region    20230321    611. 有效三角形的个数
+
+    /**
+     * https://leetcode.cn/problems/valid-triangle-number/
+     * @param nums  含非负整数的数组 nums
+     * @return  其中可以组成三角形三条边的三元组个数
+     */
+    public int triangleNumber(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int left = j + 1, right = n - 1, k = j;
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+                    if (nums[mid] < nums[i] + nums[j]) {
+                        k = mid;
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+                ans += k - j;
+            }
+        }
+        return ans;
+    }
+    //endregion
+
+    //region    20230321    658. 找到 K 个最接近的元素
+    /**
+     * https://leetcode.cn/problems/find-k-closest-elements/
+     * @param arr   一个 排序好 的数组 arr
+     * @param k 整数 k
+     * @param x 整数 x
+     * @return  数组中找到最靠近 x（两数之差最小）的 k 个数
+     */
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int right = binarySearch1(arr, x);
+        int left = right - 1;
+        while (k-- > 0) {
+            if (left < 0) {
+                right++;
+            } else if (right >= arr.length) {
+                left--;
+            } else if (x - arr[left] <= arr[right] - x) {
+                left--;
+            } else {
+                right++;
+            }
+        }
+        List<Integer> ans = new ArrayList<Integer>();
+        for (int i = left + 1; i < right; i++) {
+            ans.add(arr[i]);
+        }
+        return ans;
+    }
+
+    public int binarySearch1(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= x) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    //endregion
+
+    //region    20230321    1894. 找到需要补充粉笔的学生编号
+
+    /**
+     * https://leetcode.cn/problems/find-the-student-that-will-replace-the-chalk/
+     *
+     * @param chalk 长度为 n 且下标从 0 开始的整数数组 chalk
+     * @param k     一个整数 k
+     * @return
+     */
+    public int chalkReplacer(int[] chalk, int k) {
+        int n = chalk.length;
+        if (chalk[0] > k) {
+            return 0;
+        }
+        for (int i = 1; i < n; i++) {
+            chalk[i] += chalk[i - 1];
+            if (chalk[i] > k) {
+                return i;
+            }
+        }
+        k %= chalk[n - 1];
+        return binarySearch(chalk, k);
+    }
+
+    public int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    //endregion
+
     public static void main(String[] args) {
-        new TaegetBinarySearchBasic().minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3});
+        new TaegetBinarySearchBasic().chalkReplacer(new int[]{3, 4, 1, 2}, 25);
     }
 }
