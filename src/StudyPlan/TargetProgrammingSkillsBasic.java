408d66d760d543353cbfe424524d6d68c2593868
someday <644631077@qq.com>
2023-04-06 00:13:15 +0800

    学习计划_编程能力基础： 110. 平衡二叉树 459. 重复的子字符串

--- src/StudyPlan/TargetProgrammingSkillsBasic.java
+++ src/StudyPlan/TargetProgrammingSkillsBasic.java
@@ -7,6 +7,30 @@ package StudyPlan;
  */
 public class TargetProgrammingSkillsBasic {
 
+    //region    自定义数据结构
+    /**
+     * TreeNode 二叉树
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    //endregion
+
     //region    20230405    28. 找出字符串中第一个匹配项的下标
 
     /**
@@ -31,6 +55,42 @@ public class TargetProgrammingSkillsBasic {
             }
         }
         return -1;
+    }
+    //endregion
+
+    //region    20230406    110. 平衡二叉树
+
+    /**
+     * https://leetcode.cn/problems/balanced-binary-tree/
+     *
+     * @param root 一个二叉树根节点 root
+     * @return 判断是否是平衡二叉树
+     */
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(treeDepth(root.right) - treeDepth(root.left)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int treeDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(treeDepth(root.left), treeDepth(root.right));
+    }
+    //endregion
+
+    //region    20230406    459. 重复的子字符串
+
+    /**
+     * https://leetcode.cn/problems/repeated-substring-pattern/
+     * @param s 非空的字符串 s
+     * @return  检查是否可以通过由它的一个子串重复多次构成
+     */
+    public boolean repeatedSubstringPattern(String s) {
+        String str=s+s;
+        return str.substring(1,str.length()-1).contains(s);
     }
     //endregion
 
